6410bf3f04a96b08e0a85c5fea598bb6edf0a395
Scott Walls <scottwalls2008@gmail.com>
2022-11-05 13:08:18 -0500

    Hello world example app

--- flake.nix
+++ flake.nix
@@ -2,14 +2,24 @@
   description = "Development and deployment env for rusty-trombone";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    devShell = pkgs.mkShell {
-      packages = with pkgs; [
-        openssl
-        rustup
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            openssl
+            pkg-config
+            rustup
+          ];
+        };
+        shellHook = ''
+          export OPENSSL_DIR=${pkgs.openssl}
+        '';
+      });
 }
